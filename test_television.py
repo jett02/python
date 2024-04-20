2053d4a95b068c114fb8daced2ae4e7da4a39386
jett02 <jphusker9@gmail.com>
2024-04-19 22:55:42 -0500

    Create test_television.py
    file test

---
+++ test_television.py
@@ -0,0 +1,77 @@
+import pytest
+from television import Television
+
+@pytest.fixture
+def tv():
+    return Television()
+
+def test_init(tv):
+    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+
+def test_power(tv):
+    tv.power()
+    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+    tv.power()
+    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+
+def test_mute(tv):
+    tv.power()
+    tv.volume_up()
+    tv.mute()
+    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+    tv.mute()
+    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+    tv.power()
+    tv.mute()
+    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+    tv.mute()
+    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+
+
+def test_channel_up(tv):
+    tv.channel_up()
+    assert str(tv) == "Power = False, Channel = 1, Volume = 0"
+    tv.power()
+    tv.channel_up()
+    tv.channel_up()
+    tv.channel_up()
+    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+
+
+def test_channel_down(tv):
+    tv.channel_down()
+    assert str(tv) == "Power = False, Channel = 3, Volume = 0"
+    tv.power()
+    tv.channel_down()
+    tv.channel_down()
+    tv.channel_down()
+    assert str(tv) == "Power = True, Channel = 3, Volume = 0"
+
+def test_volume_up(tv):
+    tv.volume_up()
+    assert str(tv) == "Power = False, Channel = 0, Volume = 1"
+    tv.power()
+    tv.volume_up()
+    tv.volume_up()
+    tv.volume_up()
+    assert str(tv) == "Power = True, Channel = 0, Volume = 2"
+    tv.mute()
+    tv.volume_up()
+    assert str(tv) == "Power = True, Channel = 0, Volume = 1"
+
+def test_volume_down(tv):
+    tv.volume_down()
+    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+    tv.power()
+    tv.volume_up()
+    tv.volume_up()
+    tv.volume_up()
+    tv.volume_down()
+    assert str(tv) == "Power = True, Channel = 0, Volume = 1"
+    tv.mute()
+    tv.volume_down()
+    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+    tv.volume_down()
+    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+
+
